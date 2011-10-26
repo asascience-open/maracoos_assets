27cadd5568892fe8f4bea0c1913329df1d0403b4
Charlton Galvarino <charlton@2creek.com>
2011-10-26 03:00:53 -0400

    version

--- index.php
+++ index.php
@@ -244,7 +244,7 @@
     <script type="text/javascript" src="./js/jquery/jquery.flot.crosshair.js"></script>
     <script type="text/javascript" src="./js/jquery/jquery.flot.navigate.js"></script>
     <script type="text/javascript" src="./js/jquery/excanvas.js"></script>
-    <script type="text/javascript" src="misc.js?v=0"></script>
-    <script type="text/javascript" src="map.js.php?v=0"></script>
+    <script type="text/javascript" src="misc.js?v=1"></script>
+    <script type="text/javascript" src="map.js.php?v=1"></script>
   </body>
 </html>
