c660c91ccfcbd81d1e6bb0a09e98e9486565346b
Charlton Galvarino <charlton@2creek.com>
2012-01-19 10:09:19 -0500

    new hrecos sites

---
+++ popupHRECOS.php
@@ -0,0 +1,5 @@
+<?php
+  $h = '<table class="obsDetails"><tr><td style=\'text-align:center\'>Hudson River Environmental Conditions Observing System (HRECOS)<br><br></td></tr><tr><td style=\'text-align:center\'><a target=_blank href=\'http://www.hrecos.org/joomla/\'>Provider information</a></td></tr></table>';
+
+  echo json_encode(Array('html' => $h));
+?>
