8345df7289cae514363fe528cba8b21b19c4c8eb
Charlton Galvarino <charlton@2creek.com>
2012-01-31 00:49:26 -0500

    rutgers popup

--- popupGliders.php
+++ popupGliders.php
@@ -59,6 +59,29 @@
       array_push($o,"<tr><td colspan=2><table>"."<tr>".implode('</tr><tr>',$tr)."</tr>"."</table></td></tr>");
       array_push($o,"<tr><td colspan=2 style='text-align:center'><font color='gray'>Mouseover a thumbnail to view a larger image.</font></td></tr>");
     }
+    else if (preg_match('/rutgers/',$_REQUEST['u'])) {
+      $html = file_get_contents($_REQUEST['u']);
+      preg_match_all('/<img src="([^"]*cross_sections[^"]*)"/',$html,$matches);
+      $td = array();
+      $tr = array();
+      for ($i = 0; $i < count($matches[1]); $i++) {
+        $img = $matches[1][$i];
+        $big_img = preg_replace('/_tn/','_lores',$img);
+        array_push($td
+          ,'<a onmouseover="overlib(\'<table><tr><td><img src='
+            .$big_img
+            .'></td></tr></table>\',VAUTO,HAUTO,FGCOLOR,\'#8EAACE\')"><img width=25 src="'
+            .$img
+          .'" onmouseout=\'nd()\' ></a>'
+        );
+        if (($i + 1) % 8 == 0 || $i == count($matches[1]) - 1) {
+          array_push($tr,'<td>'.implode('</td><td>',$td).'</td>');
+          $td = array();
+        }
+      }
+      array_push($o,"<tr><td colspan=2><table>"."<tr>".implode('</tr><tr>',$tr)."</tr>"."</table></td></tr>");
+      array_push($o,"<tr><td colspan=2 style='text-align:center'><font color='gray'>Mouseover a thumbnail to view a larger image.</font></td></tr>");
+    }
     array_push($o,"<tr><td colspan=2 style='text-align:center'><a target=_blank href='".$_REQUEST['u']."'>More observations and glider information</a></td></tr>");
   }
 
