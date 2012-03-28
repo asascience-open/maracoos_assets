be0df6399ab38a367f4822831fdaf1f787cc65b6
Charlton Galvarino <charlton@2creek.com>
2012-03-28 10:32:38 -0400

    header for IE

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
 
   if ($_REQUEST['config'] == 'ecop' && !checkLoggedin()) {
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
   <head>
     <title><?php echo $title?></title>
