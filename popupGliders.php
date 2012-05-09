e0308e5abd9f97459f59caed59815e81adf63802
Charlton Galvarino <charlton@2creek.com>
2012-05-09 13:12:17 -0400

    look for 'act' if scripps fails and also pull back headers

--- popupGliders.php
+++ popupGliders.php
@@ -1,45 +1,22 @@
 <?php
+  $requestUrl = $_REQUEST['u'];
   $o = array(
     sprintf("<tr><td colspan=2 style='text-align:center'><b>%s</b></td></tr>",$_REQUEST['t'])
   );
-  if (isset($_REQUEST['u']) && $_REQUEST['u'] != '') {
-    if (preg_match('/sccoos/',$_REQUEST['u'])) {
-      $html = @file_get_contents($_REQUEST['u']);
-      preg_match('/lastleg: (.*)/',$html,$matches);
-      $lastleg = $matches[1];
-      preg_match('/mission: "(.*)"/',$html,$matches);
-      $mission = $matches[1];
-      preg_match('/varlist: \[(.*)\]/',$html,$matches);
-      $varlist = explode(',',preg_replace('/"| /','',$matches[1]));
-      // not sure why this is showing up 2x
-      array_pop($varlist);
-      preg_match('/plotRoot: "(.*)"/',$html,$matches);
-      $plotRoot = $matches[1];
-
-      $img = array();
-      for ($i = 1; $i <= $lastleg; $i++) {
-        $img[$i-1] = array();
-        foreach ($varlist as $var) {
-          array_push($img[$i-1]
-            ,'<a onmouseover="overlib(\'<table><tr><td><img src='
-              .sprintf("%s%s_%s_%s_large.png",$plotRoot,$mission,$var,$i)
-              .'></td></tr></table>\',VAUTO,HAUTO,FGCOLOR,\'#8EAACE\')"><img height=19 src="'
-              .sprintf("%s%s_%s_%s.png",$plotRoot,$mission,$var,$i)
-            .'" onmouseout=\'nd()\' ></a>');
-        }
+  if (isset($requestUrl) && $requestUrl != '') {
+    if (preg_match('/sccoos/',$requestUrl)) {
+      $a = getSCOOS($requestUrl);
+      if (count($a) == 0) {
+        $a = getSCOOS($requestUrl.'act'); 
+        $requestUrl .= 'act';
       }
-      $rows = '';
-      for ($i = 0; $i < count($img); $i++) {
-        $rows .= '<tr><td>'.implode('</td><td>',$img[$i]).'</td></tr>'."\n";
-      }
-      if ($rows != '') {
-        array_push($o,"<tr><td colspan=2><table>".$rows."</table></td></tr>");
-        array_push($o,"<tr><td colspan=2 style='text-align:center'><font color='gray'>Mouseover a thumbnail to view a larger image.</font></td></tr>");
+      if (count($a) == 2) {
+        array_push($o,$a[0],$a[1]);
       }
     }
-    else if (preg_match('/washington/',$_REQUEST['u'])) {
-      $u = substr($_REQUEST['u'],0,strrpos($_REQUEST['u'],'/') + 1);
-      $html = file_get_contents($_REQUEST['u']);
+    else if (preg_match('/washington/',$requestUrl)) {
+      $u = substr($requestUrl,0,strrpos($requestUrl,'/') + 1);
+      $html = file_get_contents($requestUrl);
       preg_match_all('/<img src="(pngplot[^"]*)"/',$html,$matches);
       $td = array();
       $tr = array();
@@ -63,8 +40,8 @@
         array_push($o,"<tr><td colspan=2 style='text-align:center'><font color='gray'>Mouseover a thumbnail to view a larger image.</font></td></tr>");
       }
     }
-    else if (preg_match('/rutgers/',$_REQUEST['u'])) {
-      $html = file_get_contents($_REQUEST['u']);
+    else if (preg_match('/rutgers/',$requestUrl)) {
+      $html = file_get_contents($requestUrl);
       preg_match_all('/<img src="([^"]*cross_sections[^"]*)"/',$html,$matches);
       $td = array();
       $tr = array();
@@ -88,8 +65,52 @@
         array_push($o,"<tr><td colspan=2 style='text-align:center'><font color='gray'>Mouseover a thumbnail to view a larger image.</font></td></tr>");
       }
     }
-    array_push($o,"<tr><td colspan=2 style='text-align:center'><a target=_blank href='".$_REQUEST['u']."'>More observations and glider information</a></td></tr>");
+    array_push($o,"<tr><td colspan=2 style='text-align:center'><a target=_blank href='".$requestUrl."'>More observations and glider information</a></td></tr>");
   }
 
   echo json_encode(Array('html' => '<table class="obsDetails">'.implode('',$o).'</table>'));
+
+  function getSCOOS($u) {
+    $html = @file_get_contents($u);
+    preg_match('/lastleg: (.*)/',$html,$matches);
+    $lastleg = $matches[1];
+    preg_match('/mission: "(.*)"/',$html,$matches);
+    $mission = $matches[1];
+    preg_match('/varlist: \[(.*)\]/',$html,$matches);
+    $varlist = explode(',',preg_replace('/"| /','',$matches[1]));
+    // not sure why this is showing up 2x
+    array_pop($varlist);
+    preg_match('/plotRoot: "(.*)"/',$html,$matches);
+    $plotRoot = $matches[1];
+
+    $img = array();
+    for ($i = 1; $i <= $lastleg; $i++) {
+      $img[$i-1] = array();
+      foreach ($varlist as $var) {
+        $c = get_headers(sprintf("%s%s_%s_%s_large.png",$plotRoot,$mission,$var,$i));
+        $notFound = false;
+        for ($j = 0; $j < count($c); $j++) {
+          $notFound = $notFound || preg_match('/404 Not Found/i',$c[$j]);
+        }
+        if (!$notFound) {
+          array_push($img[$i-1]
+            ,'<a onmouseover="overlib(\'<table><tr><td><img src='
+              .sprintf("%s%s_%s_%s_large.png",$plotRoot,$mission,$var,$i)
+              .'></td></tr></table>\',VAUTO,HAUTO,FGCOLOR,\'#8EAACE\')"><img height=19 src="'
+              .sprintf("%s%s_%s_%s.png",$plotRoot,$mission,$var,$i)
+            .'" onmouseout=\'nd()\' ></a>');
+        }
+      }
+    }
+    $rows = '';
+    for ($i = 0; $i < count($img); $i++) {
+      $rows .= '<tr><td>'.implode('</td><td>',$img[$i]).'</td></tr>'."\n";
+    }
+    $o = array();
+    if ($rows != '') {
+      array_push($o,"<tr><td colspan=2><table>".$rows."</table></td></tr>");
+      array_push($o,"<tr><td colspan=2 style='text-align:center'><font color='gray'>Mouseover a thumbnail to view a larger image.</font></td></tr>");
+    }
+    return $o;
+  }
 ?>
