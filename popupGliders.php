af3fc2f83455d7ffb255b25670c53af2e8b28a91
Charlton Galvarino <charlton@2creek.com>
2012-05-09 11:51:39 -0400

    fix new format for grabbing thumbs

--- popupGliders.php
+++ popupGliders.php
@@ -5,18 +5,16 @@
   if (isset($_REQUEST['u']) && $_REQUEST['u'] != '') {
     if (preg_match('/sccoos/',$_REQUEST['u'])) {
       $html = @file_get_contents($_REQUEST['u']);
-      preg_match('/var lastleg = (.*);/',$html,$matches);
+      preg_match('/lastleg: (.*)/',$html,$matches);
       $lastleg = $matches[1];
-      preg_match('/var mission = "(.*)"/',$html,$matches);
+      preg_match('/mission: "(.*)"/',$html,$matches);
       $mission = $matches[1];
-      preg_match('/varlist = \[(.*)\]/',$html,$matches);
+      preg_match('/varlist: \[(.*)\]/',$html,$matches);
       $varlist = explode(',',preg_replace('/"| /','',$matches[1]));
       // not sure why this is showing up 2x
       array_pop($varlist);
-      preg_match('/var plotbase = "(.*)"/',$html,$matches);
-      $plotbase = $matches[1];
-      preg_match('/var largeplotbase = "(.*)"/',$html,$matches);
-      $largeplotbase = $matches[1];
+      preg_match('/plotRoot: "(.*)"/',$html,$matches);
+      $plotRoot = $matches[1];
 
       $img = array();
       for ($i = 1; $i <= $lastleg; $i++) {
@@ -24,9 +22,9 @@
         foreach ($varlist as $var) {
           array_push($img[$i-1]
             ,'<a onmouseover="overlib(\'<table><tr><td><img src='
-              .sprintf($largeplotbase,$mission,$var,$i)
+              .sprintf("%s%s_%s_%s_large.png",$plotRoot,$mission,$var,$i)
               .'></td></tr></table>\',VAUTO,HAUTO,FGCOLOR,\'#8EAACE\')"><img height=19 src="'
-              .sprintf($plotbase,$mission,$var,$i)
+              .sprintf("%s%s_%s_%s.png",$plotRoot,$mission,$var,$i)
             .'" onmouseout=\'nd()\' ></a>');
         }
       }
