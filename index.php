395763239092cda4a8ed4f8d3adea65c25d8313f
Charlton Galvarino <charlton@2creek.com>
2013-09-18 09:37:30 -0400

    use DNS for glider json

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   require_once('auth.php');
 
   $title  = 'MARACOOS Assets';
-  $version = '0.003';
+  $version = '0.004';
 
   $config = isset($_REQUEST['config']) ? $_REQUEST['config'] : (getenv('config') ? getenv('config') : null);
   $_SESSION['config'] = $config;
