62c4f882538b43fcae07bb19f60db6f73dae5607
Charlton Galvarino <charlton@2creek.com>
2014-12-11 10:02:40 -0500

    drifter metadata

--- popupDrifters.php
+++ popupDrifters.php
@@ -5,7 +5,7 @@
     sprintf("<tr><td colspan=2 style='text-align:center'><b>%s</b></td></tr>",$_REQUEST['t'])
   );
   if (isset($_REQUEST['u'])) {
-    array_push($o,"<tr><td colspan=2 style='text-align:center'><br>Archived data is served via OPeNDAP. For details, call 508-495-2211 or email james.manning@noaa.gov.<br><br></td></tr>");
+    array_push($o,"<tr><td colspan=2 style='text-align:center'><br>Archived data is served via OPeNDAP. For details, call 508-566-4080 or email james.manning@noaa.gov.<br><br></td></tr>");
     array_push($o,"<tr><td colspan=2 style='text-align:center'><a target=_blank href='http://www.nefsc.noaa.gov/drifter'>More observations and drifter information</a></td></tr>");
   }
 
